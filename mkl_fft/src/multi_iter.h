6b8fb54d52ebec22295df313fefff27556c5c66e
Oleksandr Pavlyk <oleksandr.pavlyk@intel.com>
2019-02-06 09:26:22 -0600

    ENH: Perf. improv. for ND fft on real arrays
    Intel (R) MKL produces only half harmonics for real input arrays,
    and copying is done in mkl_fft, by using multi iterators.
    
    The multi-iterator used to reside in the separate compilation unit
    which was preventing some compilers from inlining calls.
    
    Implementation was moved from .c file into header.
    
    Additionally, the multi-iterator structs are now not allocated on
    the heap.

--- mkl_fft/src/multi_iter.h
+++ mkl_fft/src/multi_iter.h
@@ -29,6 +29,9 @@
 #define MULTI_ITER_H
 
 #include "numpy/arrayobject.h"
+#include "mkl.h"
+#include <string.h>
+#include <assert.h>
 
 typedef struct multi_iter_t {
   npy_intp *shape; // shape of the tensor
@@ -47,19 +50,19 @@ typedef struct multi_iter_masked_t {
 } multi_iter_masked_t;
 
 
-#define MultiIter_Index(mit) ((mit)->ind)
-#define MultiIter_Shape(mit) ((mit)->shape)
-#define MultiIter_Done(mit)  ((mit)->done)
-#define MultiIter_Rank(mit)  ((mit)->rank)
-#define MultiIter_Mask(mit)  ((mit)->mask)
-#define MultiIter_MaskLength(mit)  ((mit)->mask_len)
+#define MultiIter_Index(mit) ((mit).ind)
+#define MultiIter_Shape(mit) ((mit).shape)
+#define MultiIter_Done(mit)  ((mit).done)
+#define MultiIter_Rank(mit)  ((mit).rank)
+#define MultiIter_Mask(mit)  ((mit).mask)
+#define MultiIter_MaskLength(mit)  ((mit).mask_len)
 
-#define MultiIter_IndexElem(mit, i) ((mit)->ind)[(i)]
-#define MultiIter_ShapeElem(mit, i) ((mit)->shape)[(i)]
-#define MultiIter_MaskElem(mit, i) ((mit)->mask)[(i)]
+#define MultiIter_IndexElem(mit, i) ((mit).ind)[(i)]
+#define MultiIter_ShapeElem(mit, i) ((mit).shape)[(i)]
+#define MultiIter_MaskElem(mit, i) ((mit).mask)[(i)]
 
-multi_iter_t* multi_iter_new(npy_intp*, int);
-multi_iter_masked_t* multi_iter_masked_new(npy_intp*, int, int*, int);
+void multi_iter_new(multi_iter_t*, npy_intp*, int);
+void multi_iter_masked_new(multi_iter_masked_t*, npy_intp*, int, int*, int);
 
 void multi_iter_free(multi_iter_t*);
 int multi_iter_next(multi_iter_t*);
@@ -67,5 +70,131 @@ int multi_iter_next(multi_iter_t*);
 void multi_iter_masked_free(multi_iter_masked_t*);
 int multi_iter_masked_next(multi_iter_masked_t*);
 
+inline void
+multi_iter_new(multi_iter_t* mi, npy_intp shape[], int rank) {
+    int i;
+    char d = 0;
+
+    assert(rank > 0);
+
+    MultiIter_Index(*mi) = (npy_intp *) mkl_calloc(rank,  sizeof(npy_intp), 64);
+    MultiIter_Shape(*mi) = (npy_intp *) mkl_malloc(rank * sizeof(npy_intp), 64);
+    memcpy(MultiIter_Shape(*mi), shape, rank * sizeof(npy_intp));
+    MultiIter_Rank(*mi) = rank;
+
+    for(i=0; i < rank; i++) {
+	d |= MultiIter_IndexElem(*mi, i) >= MultiIter_ShapeElem(*mi, i);
+	if (d) break;
+    }
+
+    MultiIter_Done(*mi) = d;
+
+    return;
+}
+
+inline void
+multi_iter_masked_new(
+    multi_iter_masked_t* mi, npy_intp shape[], int rank, int mask[], int mask_len) 
+{
+    int i;
+    char d = 0;
+
+    assert(rank > 0);
+
+    MultiIter_Index(*mi) = (npy_intp *) mkl_calloc(rank,  sizeof(npy_intp), 64);
+    MultiIter_Shape(*mi) = (npy_intp *) mkl_malloc(rank * sizeof(npy_intp), 64);
+    memcpy(MultiIter_Shape(*mi), shape, rank * sizeof(npy_intp));
+    MultiIter_Rank(*mi) = rank;
+
+    for(i=0; i < rank; i++) {
+	d |= MultiIter_IndexElem(*mi, i) >= MultiIter_ShapeElem(*mi, i);
+	if (d) break;
+    }
+
+    MultiIter_Done(*mi) = d;
+
+    assert(mask_len > 0);
+    MultiIter_MaskLength(*mi) = mask_len;
+    MultiIter_Mask(*mi) = (int *) mkl_malloc(mask_len * sizeof(int), 64);
+    memcpy(MultiIter_Mask(*mi), mask, mask_len * sizeof(int));
+
+    return;
+}
+
+
+inline void
+multi_iter_masked_free(multi_iter_masked_t *mi) {
+    if (mi) {
+	if(MultiIter_Index(*mi))
+	    mkl_free(MultiIter_Index(*mi));
+
+	if (MultiIter_Shape(*mi))
+	    mkl_free(MultiIter_Shape(*mi));
+
+	if (MultiIter_Mask(*mi))
+	    mkl_free(MultiIter_Mask(*mi));
+    }
+
+    return;
+}
+
+inline void
+multi_iter_free(multi_iter_t *mi) {
+    if (mi) {
+	if(MultiIter_Index(*mi))
+	    mkl_free(MultiIter_Index(*mi));
+
+	if (MultiIter_Shape(*mi))
+	    mkl_free(MultiIter_Shape(*mi));
+
+    }
+
+    return;
+}
+
+
+/* Modifies iterator in-place, returns 1 when iterator is empty, 0 otherwise */
+inline int
+multi_iter_next(multi_iter_t *mi) {
+    int j, k;
+
+    if(MultiIter_Done(*mi)) return 1;
+  
+    for(k = MultiIter_Rank(*mi); k > 0; k--) {
+	j = k-1;
+	if (++(MultiIter_IndexElem(*mi, j)) < MultiIter_ShapeElem(*mi, j))
+	    return 0;
+	else {
+	    MultiIter_IndexElem(*mi, j) = 0;
+	    if (!j) {
+		MultiIter_Done(*mi) = 1;
+	    }
+	}
+    }
+
+    return 1;
+}
+
+/* Modifies iterator in-place, returns 1 when iterator is empty, 0 otherwise */
+inline int
+multi_iter_masked_next(multi_iter_masked_t *mi) {
+    int j, k;
+
+    if(MultiIter_Done(*mi)) return 1;
+
+    for(k = MultiIter_MaskLength(*mi); k >0; k--) {
+	j = MultiIter_MaskElem(*mi, k - 1);
+	if (++(MultiIter_IndexElem(*mi, j)) < MultiIter_ShapeElem(*mi, j))
+	    return 0;
+	else {
+	    MultiIter_IndexElem(*mi, j) = 0;
+	    if (!k) {
+		MultiIter_Done(*mi) = 1;
+	    }
+	}
+    }
+
+    return 1;
+}
 
 #endif
