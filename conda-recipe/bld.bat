26edc86566fd5d8b7faff1f959132d3703ad5e8c
Komarova, Evseniia <evseniia.komarova@intel.com>
2025-08-11 17:57:30 +0200

    Fix windows build

--- conda-recipe/bld.bat
+++ conda-recipe/bld.bat
@@ -1,7 +1,5 @@
 set MKLROOT=%PREFIX%
 
-rem Workaround to remove spaces from the env value
-set WHEELS_OUTPUT_FOLDER=%WHEELS_OUTPUT_FOLDER: =%
 rem Build wheel package
 if NOT "%WHEELS_OUTPUT_FOLDER%"=="" (
     %PYTHON% -m pip wheel --no-build-isolation --no-deps .
