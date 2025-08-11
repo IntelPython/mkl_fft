0893762e66901c747e3129f1a086a924aefd3199
Komarova, Evseniia <evseniia.komarova@intel.com>
2025-08-11 17:31:48 +0200

    Fix pre-commit test issues

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -10,8 +10,8 @@ read -r GLIBC_MAJOR GLIBC_MINOR <<<"$(conda list '^sysroot_linux-64$' \
 # Build wheel package
 if [ -n "${WHEELS_OUTPUT_FOLDER}" ]; then
      $PYTHON -m pip wheel --no-build-isolation --no-deps .
-     ${PYTHON} -m wheel tags --remove --platform-tag ${WHEELS_BUILD_TAG} mkl_fft*.whl
-     cp mkl_fft*.whl ${WHEELS_OUTPUT_FOLDER}
+     ${PYTHON} -m wheel tags --remove --platform-tag "${WHEELS_BUILD_TAG}" mkl_fft*.whl
+     cp mkl_fft*.whl "${WHEELS_OUTPUT_FOLDER}"
 else
     # Build conda package
     $PYTHON -m pip install --no-build-isolation --no-deps .
