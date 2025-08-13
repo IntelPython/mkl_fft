step-security-bot/IntelPython_mkl_fft

get rid of overwrite_x kwarg in mkl_fft, instead utilize out kwarg

mkl_fft/interfaces/_scipy_fft.py
@@ -256,6 +256,17 @@ def _init_nd_shape_and_axes(x, shape, axes, invreal=False):
     return tuple(shape), list(axes)
 
 
+def _use_input_as_out(x, overwrite_x):
+    """Check if the input can be used as output."""
+    if overwrite_x and np.issubdtype(x.dtype, np.complexfloating):
+        # pass input as out to overwrite it
+        out = x
+    else:
+        out = None
+
+    return out
+
+
 def _validate_input(x):
     try:
         x = _supported_array_or_not_implemented(x)
@@ -276,12 +287,11 @@ def fft(
     """
     _check_plan(plan)
     x = _validate_input(x)
+    out = _use_input_as_out(x, overwrite_x)
     fsc = _compute_fwd_scale(norm, n, x.shape[axis])
 
     with _Workers(workers):
-        return mkl_fft.fft(
-            x, n=n, axis=axis, overwrite_x=overwrite_x, fwd_scale=fsc
-        )
+        return mkl_fft.fft(x, n=n, axis=axis, fwd_scale=fsc, out=out)
 
 
 def ifft(
@@ -295,12 +305,11 @@ def ifft(
     """
     _check_plan(plan)
     x = _validate_input(x)
+    out = _use_input_as_out(x, overwrite_x)
     fsc = _compute_fwd_scale(norm, n, x.shape[axis])
 
     with _Workers(workers):
-        return mkl_fft.ifft(
-            x, n=n, axis=axis, overwrite_x=overwrite_x, fwd_scale=fsc
-        )
+        return mkl_fft.ifft(x, n=n, axis=axis, fwd_scale=fsc, out=out)
 
 
 def fft2(
@@ -375,13 +384,12 @@ def fftn(
     """
     _check_plan(plan)
     x = _validate_input(x)
+    out = _use_input_as_out(x, overwrite_x)
     s, axes = _init_nd_shape_and_axes(x, s, axes)
     fsc = _compute_fwd_scale(norm, s, x.shape)
 
     with _Workers(workers):
-        return mkl_fft.fftn(
-            x, s=s, axes=axes, overwrite_x=overwrite_x, fwd_scale=fsc
-        )
+        return mkl_fft.fftn(x, s=s, axes=axes, fwd_scale=fsc, out=out)
 
 
 def ifftn(
@@ -402,13 +410,12 @@ def ifftn(
     """
     _check_plan(plan)
     x = _validate_input(x)
+    out = _use_input_as_out(x, overwrite_x)
     s, axes = _init_nd_shape_and_axes(x, s, axes)
     fsc = _compute_fwd_scale(norm, s, x.shape)
 
     with _Workers(workers):
-        return mkl_fft.ifftn(
-            x, s=s, axes=axes, overwrite_x=overwrite_x, fwd_scale=fsc
-        )
+        return mkl_fft.ifftn(x, s=s, axes=axes, fwd_scale=fsc, out=out)
 
 
 def rfft(

IntelPython/mkl_fft

mkl_fft/interfaces/_scipy_fft.py
@@ -203,6 +203,17 @@ def _init_nd_shape_and_axes(x, shape, axes, invreal=False):
     return tuple(shape), list(axes)
 
 
+def _use_input_as_out(x, overwrite_x):
+    """Check if the input can be used as output."""
+    if overwrite_x and np.issubdtype(x.dtype, np.complexfloating):
+        # pass input as out to overwrite it
+        out = x
+    else:
+        out = None
+
+    return out
+
+
 def _validate_input(x):
     try:
         x = _supported_array_or_not_implemented(x)
@@ -223,11 +234,10 @@ def fft(
     """
     _check_plan(plan)
     x = _validate_input(x)
+    out = _use_input_as_out(x, overwrite_x)
 
     with _Workers(workers):
-        return mkl_fft.fft(
-            x, n=n, axis=axis, overwrite_x=overwrite_x, norm=norm
-        )
+        return mkl_fft.fft(x, n=n, axis=axis, norm=norm, out=out)
 
 
 def ifft(
@@ -241,11 +251,10 @@ def ifft(
     """
     _check_plan(plan)
     x = _validate_input(x)
+    out = _use_input_as_out(x, overwrite_x)
 
     with _Workers(workers):
-        return mkl_fft.ifft(
-            x, n=n, axis=axis, overwrite_x=overwrite_x, norm=norm
-        )
+        return mkl_fft.ifft(x, n=n, axis=axis, norm=norm, out=out)
 
 
 def fft2(
@@ -320,12 +329,11 @@ def fftn(
     """
     _check_plan(plan)
     x = _validate_input(x)
+    out = _use_input_as_out(x, overwrite_x)
     s, axes = _init_nd_shape_and_axes(x, s, axes)
 
     with _Workers(workers):
-        return mkl_fft.fftn(
-            x, s=s, axes=axes, overwrite_x=overwrite_x, norm=norm
-        )
+        return mkl_fft.fftn(x, s=s, axes=axes, norm=norm, out=out)
 
 
 def ifftn(
@@ -346,12 +354,11 @@ def ifftn(
     """
     _check_plan(plan)
     x = _validate_input(x)
+    out = _use_input_as_out(x, overwrite_x)
     s, axes = _init_nd_shape_and_axes(x, s, axes)
 
     with _Workers(workers):
-        return mkl_fft.ifftn(
-            x, s=s, axes=axes, overwrite_x=overwrite_x, norm=norm
-        )
+        return mkl_fft.ifftn(x, s=s, axes=axes, norm=norm, out=out)
 
 
 def rfft(