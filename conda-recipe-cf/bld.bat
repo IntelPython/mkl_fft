81591cb4fb65a1be1594d212d60a898c12d3f3e1
Nikita Grigorian <nikita.grigorian@intel.com>
2026-03-05 21:34:32 -0800

    align conda-forge recipe with normal recipe

--- conda-recipe-cf/bld.bat
+++ conda-recipe-cf/bld.bat
@@ -1,3 +1,13 @@
 set MKLROOT=%PREFIX%
-%PYTHON% -m pip install --no-build-isolation --no-deps .
-if errorlevel 1 exit 1
+
+rem Build wheel package
+if NOT "%WHEELS_OUTPUT_FOLDER%"=="" (
+    %PYTHON% -m pip wheel --no-build-isolation --no-deps .
+    if errorlevel 1 exit 1
+    copy mkl_fft*.whl %WHEELS_OUTPUT_FOLDER%
+    if errorlevel 1 exit 1
+) ELSE (
+    rem Build conda package
+    %PYTHON% -m pip install --no-build-isolation --no-deps .
+    if errorlevel 1 exit 1
+)
