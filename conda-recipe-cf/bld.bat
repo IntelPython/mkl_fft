3d304c4e3c7e41dd2b73d68714e28d6042fc7dc0
Nikita Grigorian <nikita.grigorian@intel.com>
2026-03-09 13:53:18 -0700

    align conda-forge workflow recipe with feedstock recipe

--- conda-recipe-cf/bld.bat
+++ conda-recipe-cf/bld.bat
@@ -1,13 +1,3 @@
 set MKLROOT=%PREFIX%
-
-rem Build wheel package
-if NOT "%WHEELS_OUTPUT_FOLDER%"=="" (
-    %PYTHON% -m pip wheel --no-build-isolation --no-deps .
-    if errorlevel 1 exit 1
-    copy mkl_fft*.whl %WHEELS_OUTPUT_FOLDER%
-    if errorlevel 1 exit 1
-) ELSE (
-    rem Build conda package
-    %PYTHON% -m pip install --no-build-isolation --no-deps .
-    if errorlevel 1 exit 1
-)
+%PYTHON% setup.py build --force install --old-and-unmanageable
+if errorlevel 1 exit 1
