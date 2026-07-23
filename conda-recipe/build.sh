f6d7e2a68f8bbfb6b8ab85689fbada496429ee7a
Vladislav Perevezentsev <vladislav.perevezentsev@intel.com>
2026-07-23 08:18:42 -0700

    Remove redundant LDFLAGS from build.sh

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -5,7 +5,6 @@ if [ -d "build" ]; then
 fi
 
 export CFLAGS="-I$PREFIX/include $CFLAGS"
-export LDFLAGS="-L${PREFIX}/lib ${LDFLAGS}"
 
 read -r GLIBC_MAJOR GLIBC_MINOR <<<"$(conda list '^sysroot_linux-64$' \
     | tail -n 1 | awk '{print $2}' | grep -oP '\d+' | head -n 2 | tr '\n' ' ')"
