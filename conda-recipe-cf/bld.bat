f5f4213c0afaab31f60658d6940229277a3dc842
Vladislav Perevezentsev <vladislav.perevezentsev@intel.com>
2026-07-27 04:21:38 -0700

    Switch MKL_LINK to sdl and remove meson.options

--- conda-recipe-cf/bld.bat
+++ conda-recipe-cf/bld.bat
@@ -1,4 +1,2 @@
-rem intel_thread (default) is safe on conda-forge: llvm-openmp ships the Intel OpenMP
-rem runtime library directly so MKL finds the correct OpenMP runtime at runtime.
 %PYTHON% -m pip install --no-build-isolation --no-deps .
 if errorlevel 1 exit 1
--- conda-recipe-cf/build.sh
+++ conda-recipe-cf/build.sh
@@ -1,6 +1,3 @@
 #!/bin/bash -ex
 
-# gnu_thread is safe on conda-forge: llvm-openmp provides the GNU OpenMP symbols
-# (libgomp) in addition to its own so MKL finds the correct OpenMP runtime at runtime.
-# See: https://conda-forge.org/docs/maintainer/knowledge_base/#openmp
-$PYTHON -m pip install --no-build-isolation --no-deps -Csetup-args="-Dmkl_threading=gnu_thread" .
+$PYTHON -m pip install --no-build-isolation --no-deps .
